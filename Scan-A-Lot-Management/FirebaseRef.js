845aeef8029aece9323b4c6990797318a8ca2d18
Jeremy4456 <Jeremycapella@gmail.com>
2023-01-30 20:39:17 -0500

    testing js files not sure how to link it to html.

--- Scan-A-Lot-Management/FirebaseRef.js
+++ Scan-A-Lot-Management/FirebaseRef.js
@@ -1,24 +1,41 @@
-const officerList = document.querySelector('#officer-list');
+import { initializeApp } from "firebase/app "
+const firebaseConfig = {
+    apiKey: "AIzaSyDIEtCfoSgt-Ka56fFwouFDvEXId0Xrk78",
+    authDomain: "scan-a-lot.firebaseapp.com",
+    projectId: "scan-a-lot",
+    storageBucket: "scan-a-lot.appspot.com",
+    messagingSenderId: "816922417821",
+    appId: "1:816922417821:web:ba0a3811f84a8cbbacd274",
+    measurementId: "G-LVFHL8LFNV"
+  };
 
-function renderCafe(doc){
-    let li = document.createElement('li');
-    let Firstname = document.createElement('span');
-    let Lastname = document.createElement('span');
-    let Password = document.createElement('span');
-    let Username = document.createElement('span');
+  const app = initializeApp(firebaseConfig);
+// const officerList = document.querySelector('#officer-list');
 
-    li.setAttribute('data-id', doc.id);
-    Lastname.textContent = doc.data().Lastname;
-    Firstname.textContent = doc.data.Firstname;
+// function renderCafe(doc){
+//     let li = document.createElement('li');
+//     let Firstname = document.createElement('span');
+//     let Lastname = document.createElement('span');
+//     let Password = document.createElement('span');
+//     let Username = document.createElement('span');
 
-    li.appendChild(FirstName);
-    li.appendChild(Lastname);
+//     li.setAttribute('data-id', doc.id);
+//     Lastname.textContent = doc.data().Lastname;
+//     Firstname.textContent = doc.data.Firstname;
 
-    officerList.appendChild(li);
+//     li.appendChild(FirstName);
+//     li.appendChild(Lastname);
+
+//     officerList.appendChild(li);
+// }
+
+// db.collection('Officers').get().then((snapshot) => {
+//     snapshot.docs.forEach(doc => {
+//         console.log(doc.data())
+//     })
+// })
+function submitForm(e){
+    e.preventDefault();
+
+    console.log(123)
 }
-
-db.collection('Officers').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        console.log(doc.data())
-    })
-})

7172534e05d8b5af4dcaa7f5c468c1af802851cb
schrackc <112912408+schrackc@users.noreply.github.com>
2023-02-11 17:20:51 -0500

    Created js and css files along with fix tabing

---
+++ Scan-A-Lot-Management/data-table.js
@@ -0,0 +1,13 @@
+function openCity(evt, cityName) {
+    var i, tabContent, tablinks;
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
---
+++ Scan-A-Lot-Management/index.js
@@ -0,0 +1,46 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIEtCfoSgt-Ka56fFwouFDvEXId0Xrk78",
+  authDomain: "scan-a-lot.firebaseapp.com",
+  projectId: "scan-a-lot",
+  storageBucket: "scan-a-lot.appspot.com",
+  messagingSenderId: "816922417821",
+  appId: "1:816922417821:web:ba0a3811f84a8cbbacd274",
+  measurementId: "G-LVFHL8LFNV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+login.addEventListener('click',(e)=>{
+var email = document.getElementById('email').value;
+var password = document.getElementById('password').value;
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+
+    const dt = new Date();
+     update(ref(database, 'users/' + user.uid),{
+      last_login: dt,
+    })
+    //links to next page here if user is logged in. Doesnt take to next page if user fails to enter valid credentials. 
+    window.location.href='data-tables.html'
+     alert('User logged in!');
+  
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+    alert(errorMessage);
+  });
+
+});

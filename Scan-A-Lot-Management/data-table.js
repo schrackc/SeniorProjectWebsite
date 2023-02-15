23651c70c4750aacd33a180e7f41d2f9d3bf7b31
schrackc <112912408+schrackc@users.noreply.github.com>
2023-02-15 00:26:47 -0500

    Data Table Comments Added

--- Scan-A-Lot-Management/data-table.js
+++ Scan-A-Lot-Management/data-table.js
@@ -1,4 +1,5 @@
-function openCity(evt, cityName) {
+//Function makes a table visible while making the previous table invisible
+function openTable(evt, table) {
     var i, tabContent, tablinks;
     tabContent = document.getElementsByClassName("tabContent");
     for (i = 0; i < tabContent.length; i++) {
@@ -8,6 +9,6 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(table).style.display = "block";
     evt.currentTarget.className += " active";
 }

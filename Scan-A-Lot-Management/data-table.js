6e2802408f54da627bc66898c8277fde01f595af
henrybeattie2 <henry.beattie@stvincent.edu>
2023-02-14 22:58:57 -0500

    tickets tab open on start

--- Scan-A-Lot-Management/data-table.js
+++ Scan-A-Lot-Management/data-table.js
@@ -1,3 +1,19 @@
+/*$(document).ready(function() {
+  $('.my-div').show(); // or .css('display', 'block')
+
+  var i, tabContent, tablinks;
+  tabContent = document.getElementsByClassName("tabContent");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(Tickets).style.display = "block";
+  event.currentTarget.className += " active";
+}); */
+
 function openCity(evt, cityName) {          // change function name
     var i, tabContent, tablinks;
     tabContent = document.getElementsByClassName("tabContent");
@@ -11,9 +27,3 @@ function openCity(evt, cityName) {          // change function name
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
-
-
-/* $(document).ready(function() {
-  $('.my-div').show(); // or .css('display', 'block')
-}); */

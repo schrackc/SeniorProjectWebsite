5944c300d605278beb81d87705bf90d3b472fee4
schrackc <112912408+schrackc@users.noreply.github.com>
2023-02-15 00:42:38 -0500

    Merge Issue Fixes

--- Scan-A-Lot-Management/data-table.js
+++ Scan-A-Lot-Management/data-table.js
@@ -1,8 +1,5 @@
 //Function makes a table visible while making the previous table invisible
 function openTable(evt, table) {
-/*$(document).ready(function() {
-  $('.my-div').show(); // or .css('display', 'block')
-
   var i, tabContent, tablinks;
   tabContent = document.getElementsByClassName("tabContent");
   for (i = 0; i < tabContent.length; i++) {
@@ -12,6 +9,6 @@ function openTable(evt, table) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(Tickets).style.display = "block";
-  event.currentTarget.className += " active";
-}); */
+  document.getElementById(table).style.display = "block";
+  evt.currentTarget.className += " active";
+}

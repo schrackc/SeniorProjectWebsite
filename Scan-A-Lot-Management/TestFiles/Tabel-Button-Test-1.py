6d69ee80ef5edbce1aaf4e1519ca0a75d24495a2
schrackc <112912408+schrackc@users.noreply.github.com>
2023-02-20 23:45:22 -0500

    Table Button Test Complete

--- Scan-A-Lot-Management/TestFiles/Tabel-Button-Test-1.py
+++ Scan-A-Lot-Management/TestFiles/Tabel-Button-Test-1.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import time
 options = Options()
 options.page_load_strategy = 'normal'
 driver = webdriver.Chrome(options=options)
@@ -15,13 +16,14 @@ driver.get("https://scan-a-lot-management.firebaseapp.com/data-tables.html")
 
 # Find the button and wait until it is clickable
 #button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "Officers")))
-buttonValue=driver.find_element(by=By.XPATH, value='/html/body/div[2]/button[1]').click()
+driver.find_element(by=By.XPATH, value='/html/body/div[2]/button[1]').click()
 
 
-# Verify that the button click had the expected result
-assert "New Page Title" in driver.title
+time.sleep(5)
 
-print(buttonValue)
+driver.find_element(by=By.XPATH, value='/html/body/div[2]/button[3]').click()
+
+time.sleep(5)
 
 # Close the browser
 driver.quit()

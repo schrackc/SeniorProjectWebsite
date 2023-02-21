8604f7e9b16d6da6d2ab190c03a8ed0da5e85e35
henrybeattie2 <henry.beattie@stvincent.edu>
2023-02-20 23:34:26 -0500

    update

--- Scan-A-Lot-Management/TestFiles/Tabel-Button-Test-1.py
+++ Scan-A-Lot-Management/TestFiles/Tabel-Button-Test-1.py
@@ -15,11 +15,13 @@ driver.get("https://scan-a-lot-management.firebaseapp.com/data-tables.html")
 
 # Find the button and wait until it is clickable
 #button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "Officers")))
-driver.find_element(by=By.XPATH, value='/html/body/div[2]/button[1]').click()
+buttonValue=driver.find_element(by=By.XPATH, value='/html/body/div[2]/button[1]').click()
 
 
 # Verify that the button click had the expected result
 assert "New Page Title" in driver.title
+
+print(buttonValue)
 
 # Close the browser
 driver.quit()
